09ed0ae2b52a994012fd381759707b4e416e277d
xxufnaviin <naviin2373@gmail.com>
2025-09-13 19:56:50 +0800

    created api endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+from google_bigquery import main as bigqueryClient
 
 app = FastAPI(title="Modular Backend API")
 
@@ -10,3 +12,20 @@ def read_root():
     """
     return {"message": "Welcome to the main backend app!"}
 
+@app.get("/trend/analytics")
+def trend_analysis():
+    query = """
+    SELECT json_data FROM analyzed_data.trend_analytics LIMIT 1
+    """
+    data = bigqueryClient.query(query)
+    # print(type(data)) # dict
+    return {"data":data,
+            "message": "ok"}
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
